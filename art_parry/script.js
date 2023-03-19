5863c3b52d4571102ce6aab232aab3d7c24a61cc
Leito131 <115036122+Leito131@users.noreply.github.com>
2023-03-20 00:32:11 +0100

    Delete art_parry directory

--- art_parry/script.js
+++
@@ -1,9 +0,0 @@
-const nav = index.getElementById("index");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    nav.classList.add("scroll");
-  } else {
-    nav.classList.remove("scroll");
-  }
-});
